103e37c836b3426cc2a785d27a773e7727b9f356
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-02 14:26:17 +0300

    Update ContactForm.jsx
    fixed errors

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import Notiflix from 'notiflix';
 import { ReactComponent as AddIcon } from '../icons/plus-user.svg';
@@ -45,12 +45,12 @@ function ContactForm({ addContact, contacts }) {
   };
 
     return (
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Text>Name</Text>
         <Input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[a-zA-Zа-яА-Я]+([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           value={name}
@@ -60,13 +60,13 @@ function ContactForm({ addContact, contacts }) {
         <Input
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           value={number}
           onChange={handleNumberChange}
         />
-        <Button onClick={handleSubmit}><AddIcon /></Button>
+        <Button type="submit"><AddIcon /></Button>
         
       </Form>
     );
