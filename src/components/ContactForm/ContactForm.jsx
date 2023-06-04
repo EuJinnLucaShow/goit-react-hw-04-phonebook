084166cf063f5a49d4f542ea6194fe7319710c77
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-04 06:33:54 +0300

    Update ContactForm.jsx
    The phonePattern = new RegExp(/^\(?[\d +\/()–-]{6,}\)?[ .\/–-]?\d+$/); must be declared as const phonePattern = /^\(?[\d +\/()–-]{6,}\)?[ .\/–-]?\d+$/;. And let resolution = inputValue && !!inputValue.match(this.phonePattern);

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -49,7 +49,7 @@ function ContactForm({ addContact }) {
         <Input
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          const phonePattern = "\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           value={number}
